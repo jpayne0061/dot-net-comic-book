452f763640f94b831e79994d068866a047df0bd1
jpayne0061 <jpayne0061@gmail.com>
2016-10-07 12:46:26 -0700

    Added first view

--- src/dotNetComicBook/Controllers/ComicBooksController.cs
+++ src/dotNetComicBook/Controllers/ComicBooksController.cs
@@ -9,25 +9,12 @@ namespace dotNetComicBook.Controllers
     public class ComicBooksController : Controller
     {
         public ActionResult Detail()
-            {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Friday)
-            {
-                return Redirect("/ComicBooks/Schmetail");
-                //return new RedirectResult("/"); ---> this object replaced by method provided by Controller class
-            }
-
-            return Content("Hello from the Content action method!");
-            //return new ContentResult()-----> this method replaced by short cut provided by controller class
-            //{
-            //    Content = "Hello from the Detail action!"
-            //};
-            
-        }
-        
-        public ActionResult Schmetail()
         {
-            return Content("you were redirected because its Friday");
-        }     
-
+            return View();
+        }
+            
     }
+        
+
+  
 }
