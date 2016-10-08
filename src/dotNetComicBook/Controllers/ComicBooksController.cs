13b410ccee34496f525f66e0c4ab48c5d27e4416
jpayne0061 <jpayne0061@gmail.com>
2016-10-08 06:22:21 -0700

    UPadted comic book detail view

--- src/dotNetComicBook/Controllers/ComicBooksController.cs
+++ src/dotNetComicBook/Controllers/ComicBooksController.cs
@@ -10,6 +10,17 @@ namespace dotNetComicBook.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.IssueNumber = 700;
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
             
