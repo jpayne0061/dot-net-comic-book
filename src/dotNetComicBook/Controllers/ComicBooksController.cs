4489f69218e0d8cd55b488bc52f5a162997f1746
jpayne0061 <jpayne0061@gmail.com>
2016-10-10 17:22:30 -0700

    Added comicBook repository

--- src/dotNetComicBook/Controllers/ComicBooksController.cs
+++ src/dotNetComicBook/Controllers/ComicBooksController.cs
@@ -11,25 +11,9 @@ namespace dotNetComicBook.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider man",
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                IssueNumber = 700,
-                Favorite = false,
-                Artists = new Artist[]
-                {
-                    new Artist() {Name="Dan Slitt", Role="Script" },
-                    new Artist() {Name="Humberto Ramos", Role="Pencils" },
-                    new Artist() {Name="Victor Olazaba", Role="Inks" },
-                    new Artist() {Name="Chris Eliopoulos", Role="Letters" },
-                    new Artist() {Name="Edgar Delado", Role="Colors" }
-                }
-                
 
-            };
 
-            return View(comicBook);
+            return null;
         }
             
     }
